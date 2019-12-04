81b6dda1f43f9edeeab22fe15c7a601cfbf118b0
austinlin <u101034004@gmail.com>
2019-12-04 18:54:49 +0800

    整理架構

--- SideProjectVidly/App_Start/RouteConfig.cs
+++ SideProjectVidly/App_Start/RouteConfig.cs
@@ -13,6 +13,14 @@ namespace SideProjectVidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+            //012Convention-Base Routing--------------------------------
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movie/released/{year}/{month}",
+            //    new { controller = "Movie", action = "ByReleaseDate"},
+            //    new { year = @"2015|2016", month = @"\d{2}" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ SideProjectVidly/Controllers/MovieController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SideProjectVidly.Models;
+using SideProjectVidly.ViewModels;
+
+namespace SideProjectVidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        [Route("movie/Rondom")]
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        //011 Action Parameters-------------------------------------------
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id = " + id);
+        //}
+
+        //// movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = -1;
+        //    }
+
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
+        //    return Content(string.Format("pageIndex = {0}& sortBy = {1}", pageIndex, sortBy));
+        //}
+        [Route("movie/released/{year:max(2021)}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
